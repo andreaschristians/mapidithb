1cc6c5de6cbbde756ba8e21320e3127d81df9072
Andreas Christian Susanto <andreaschristiansusanto.070499@gmail.com>
2019-06-12 08:38:52 +0700

    Pindah ke React sudah rapi

--- Andreas/20190529/geomapid/src/App.js
+++ Andreas/20190529/geomapid/src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Input } from 'semantic-ui-react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="Page">
+      <div id="StyleMaps">
+        <Button.Group>
+          <Button>Streets</Button>
+          <Button>Satelite</Button>
+          <Button>Hybrid</Button>
+          <Button>Terrain</Button>
+        </Button.Group>
+      </div>
+      <div id="Bottom">
+        <img src="mapid-logo.svg" align="center" height="30px" alt="Logo" padding="5px"></img>
+        <Button id="Bawah" onClick="https://www.mapid.io/">
+          <img src="geo-icon.png" height="10px" alt="Geo"></img>
+          <img src="mapid-icon.png" height="10px" alt="Mapid"></img>
+        </Button>
+        <Button id="Bawah" content='Toolbox' icon='briefcase' labelPosition='left'></Button>
+        <Button id="Bawah" content='Details' icon='bars' labelPosition='left'></Button>
+        <Button id="Bawah" content='Inspect' icon='search' labelPosition='left'></Button>
+        <Button id="Bawah" content='Navigate' icon='location arrow' labelPosition='left'></Button>
+        <Button id="Bawah" size="Large" content='Langtitude'><div id="isi"></div></Button>
+      </div>
     </div>
   );
 }
