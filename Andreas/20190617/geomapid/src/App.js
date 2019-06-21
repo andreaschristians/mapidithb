eb053f36909760d43479922c7b2b69ab2e60d5b9
Andreas Christian Susanto <andreaschristiansusanto.070499@gmail.com>
2019-06-21 17:27:42 +0700

    change style berhasil

--- Andreas/20190617/geomapid/src/App.js
+++ Andreas/20190617/geomapid/src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
-import "./App.css";
 import ReactMapGL, { Marker } from "react-map-gl";
-import { Button, Message, Popup, Label } from "semantic-ui-react";
+import { Button, Popup, Label } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import LineTo from "react-lineto";
 import * as turf from "@turf/turf";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this._distance = this._distance.bind(this);
     this._area = this._area.bind(this);
+    this._StyleChange = this._StyleChange.bind(this);
+    this._onClickMap = this._onClickMap.bind(this);
   }
 
   state = {
@@ -22,14 +24,23 @@ class App extends Component {
       zoom: 4.69,
       area: 0,
       distance: 0,
-      visible: "none"
+      MapChange: "mapbox://styles/mapbox/streets-v11",
+      clat: 0
     }
   };
+  _onClickMap(e) {
+    console.log(e.lngLat);
+    let lat = e.lnglat;
+    // var lng = e.longitude;
+    // this.setState({ clong: lng });
+    this.setState({ clat: lat });
+  }
   _area() {
     var polygon = turf.polygon([[[103, -6], [103, -2], [104, -7], [103, -6]]]);
     var areas = parseInt(turf.area(polygon) * 100) / 100;
     this.setState({ area: areas });
   }
+
   _distance() {
     var from = turf.point([107.62099, -6.9494343]);
     var to = turf.point([106.9999, -6.534343]);
@@ -37,11 +48,12 @@ class App extends Component {
     var distances = parseInt(turf.distance(from, to, options) * 100) / 100;
     this.setState({ distance: distances });
   }
-  _onClickMap(e) {
-    document.getElementById("isilang").innerHTML = e.lngLat;
-  }
-  _streetMap() {
-    // document.getElementById('mapstyle').innerHTML = "'mapbox://styles/mapbox/streets-v9'";
+
+  _StyleChange(e) {
+    console.log(e.currentTarget.value);
+    this.setState({
+      MapChange: e.currentTarget.value
+    });
   }
 
   render() {
@@ -54,7 +66,7 @@ class App extends Component {
             "pk.eyJ1IjoiYW5kcmVhc2NocmlzdGlhbiIsImEiOiJjanZ2cnZhMjg0NWtmNDN1aTMxcGphY21xIn0.CDEBH4hJPmAhRDOtzz73Mw"
           }
           onClick={this._onClickMap}
-          mapStyle="mapbox://styles/mapbox/streets-v11"
+          mapStyle={this.state.MapChange}
           onViewportChange={viewport => this.setState({ viewport })}
         >
           <Marker
@@ -113,6 +125,32 @@ class App extends Component {
           <LineTo from="B area" to="C area" />
           <LineTo from="C area" to="A area" />
         </ReactMapGL>
+        <div id="menu" style={{ position: "fixed", top: "0" }}>
+          <input
+            type="radio"
+            value="mapbox://styles/mapbox/streets-v11"
+            onChange={this._StyleChange}
+          />
+          <Label for="streets">streets</Label>
+          <input
+            type="radio"
+            value="mapbox://styles/mapbox/light-v10"
+            onChange={this._StyleChange}
+          />
+          <Label for="light">light</Label>
+          <input
+            type="radio"
+            value="mapbox://styles/mapbox/dark-v10"
+            onChange={this._StyleChange}
+          />
+          <Label for="dark">dark</Label>
+          <input
+            type="radio"
+            value="mapbox://styles/mapbox/outdoors-v11"
+            onChange={this._StyleChange}
+          />
+          <Label for="outdoors">outdoors</Label>
+        </div>
         <div id="Bottom">
           <Popup
             trigger={
@@ -140,16 +178,15 @@ class App extends Component {
                   onClick={this._area}
                 />
               </div>
+
+              <Label style={{ display: "inline-block" }}>
+                {this.state.distance}
+              </Label>
+              <Label style={{ display: "inline-block" }}>
+                {this.state.area}
+              </Label>
             </div>
           </Popup>
-
-          <Label style={{ display: "inline-block" }}>
-            {this.state.distance}
-          </Label>
-          <Label style={{ display: "inline-block" }}>{this.state.area}</Label>
-          <Button id="Bawah" size="small" content="Langtitude">
-            <div id="isilang" />
-          </Button>
         </div>
       </div>
     );
