b5d483d286b284b6efc197a355142873dc43aab5
Andreas Christian Susanto <andreaschristiansusanto.070499@gmail.com>
2019-07-10 15:59:34 +0700

    draw button sendiri, lang dan lat muncul

--- Andreas/03. July/20190705/geomapid/src/App.js
+++ Andreas/03. July/20190705/geomapid/src/App.js
@@ -5,7 +5,7 @@ import React, { Component } from "react"; //React
 import "semantic-ui-css/semantic.min.css"; //CSS Sematic
 import Draw from "@urbica/react-map-gl-draw";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
-import { Button, Popup, Label, Message, Table, Form } from "semantic-ui-react"; //Tools bantuan untuk UI
+import { Button, Label, Table, Form } from "semantic-ui-react"; //Tools bantuan untuk UI
 import MapGL, { GeolocateControl, Marker, Source } from "@urbica/react-map-gl"; //Mapbox Urbica
 
 class App extends Component {
@@ -16,17 +16,18 @@ class App extends Component {
         //untuk di Mapbox
         latitude: -6.9184,
         longitude: 107.6093,
-        zoom: 5.5,
-        arrPoint: [] //point yang di tampilkan untuk distance
+        zoom: 5.5
       },
       mapstyle: "mapbox://styles/mapbox/streets-v11",
       unshowmarker: "unhere.png",
+      unshowmarkercctv: "unhere.png",
       sumdistance: 0,
       sumarea: 0,
-      counter: 0,
-      features: [],
-      arrCoord: [],
-      display: "none"
+      features: [0,0],
+      arrCoord: [0,0],
+      arrTbl: [],
+      display: "none",
+      mode: "simple_select"
     };
     this._StyleChange = this._StyleChange.bind(this);
     this._onClickArea = this._onClickArea.bind(this);
@@ -34,8 +35,14 @@ class App extends Component {
     this._onClickDistance = this._onClickDistance.bind(this);
     this._showmarker = this._showmarker.bind(this);
     this._unshowmarker = this._unshowmarker.bind(this);
+    this._showmarkerCCTV = this._showmarkerCCTV.bind(this);
+    this._unshowmarkerCCTV = this._unshowmarkerCCTV.bind(this);
     this._disp = this._disp.bind(this);
     this._undisp = this._undisp.bind(this);
+    this._showLngLatarea = this._showLngLatarea.bind(this);
+    this._showLngLatdis = this._showLngLatdis.bind(this);
+    this._mode = this._mode.bind(this);
+    this._modearea = this._modearea.bind(this);
   }
 
   componentDidMount() {
@@ -50,6 +57,36 @@ class App extends Component {
     document.getElementById("isilang").innerHTML =
       "Long : " + lng + " Lat : " + lat;
   }
+  _showLngLatarea() {
+    var arr = [];
+    var i;
+    for (i = 1; i < this.state.arrCoord[0].length; i++) {
+      var a = parseInt(this.state.arrCoord[0][i - 1][0] * 10000) / 10000;
+      var b = parseInt(this.state.arrCoord[0][i - 1][1] * 10000) / 10000;
+      arr.push(
+        <Table.Row collapsing>
+          <Table.Cell>{a}</Table.Cell>
+          <Table.Cell>{b}</Table.Cell>
+        </Table.Row>
+      );
+    }
+    this.setState({ arrTbl: arr });
+  }
+  _showLngLatdis() {
+    var arr = [];
+    var i;
+    for (i = 0; i < this.state.arrCoord.length; i++) {
+      var a = parseInt(this.state.arrCoord[i][0] * 10000) / 10000;
+      var b = parseInt(this.state.arrCoord[i][1] * 10000) / 10000;
+      arr.push(
+        <Table.Row collapsing>
+          <Table.Cell>{a}</Table.Cell>
+          <Table.Cell>{b}</Table.Cell>
+        </Table.Row>
+      );
+    }
+    this.setState({ arrTbl: arr });
+  }
   _StyleChange(e) {
     //procedure untuk change style maps
     this.setState({
@@ -62,6 +99,14 @@ class App extends Component {
   _unshowmarker() {
     this.setState({ unshowmarker: "unhere.png" });
   }
+  _showmarkerCCTV() {
+    this.setState({
+      unshowmarkercctv: "http://202.58.207.176:8090/mjpg/video.mjpg"
+    });
+  }
+  _unshowmarkerCCTV() {
+    this.setState({ unshowmarkercctv: "unhere.png" });
+  }
   _disp() {
     this.setState({ display: "inline-block" });
   }
@@ -73,30 +118,36 @@ class App extends Component {
     var polygon = turf.polygon(arr);
     var total = parseInt(turf.area(polygon) / 10763.91) / 100;
     this.setState({ sumarea: total }); //update nilai distance
-    this.setState({ arrCoord: arr }); //update nilai distance
+    this.setState({ arrCoord: arr });
+    this._showLngLatarea();
   }
   _onClickDistance() {
     var arr = this.state.features[0].geometry.coordinates;
     var i;
-    var total=0;
+    var total = 0;
     for (i = 1; i < arr.length; i++) {
       var from = turf.point([arr[i - 1][0], arr[i - 1][1]]);
       var to = turf.point([arr[i][0], arr[i][1]]);
       var options = { units: "kilometers" }; //satuan perhitungan
       total += turf.distance(from, to, options); //menghitung jarak data 1 dan 2
-      total=parseInt(total*100)/100;
+      total = parseInt(total * 100) / 100;
       this.setState({ sumdistance: total }); //update nilai distance
       this.setState({ arrCoord: arr });
     }
+    this._showLngLatdis();
   }
-
+  _mode() {
+    this.setState({ mode: "draw_line_string" });
+  }
+  _modearea() {
+    this.setState({ mode: "draw_polygon" });
+  }
   render() {
     const data = {
       //data coordinate untuk menampilkan line
       type: "Feature",
       geometry: {
-        type: "LineString",
-        coordinates: this.state.arrCoord //ambil dari data yang ada dari procedure _onClick
+        type: "LineString"
       }
     };
 
@@ -134,16 +185,28 @@ class App extends Component {
               alt=""
             />
           </Marker>
+          <Marker longitude={115.1244} latitude={-8.6543}>
+            <img src={this.state.unshowmarkercctv} width="320" alt="" />
+          </Marker>
           <Source id="route" type="geojson" data={data} />
           <Draw
+            mode={this.state.mode}
             onDrawCreate={({ features }) => this.setState({ features })}
-            onDrawUpdate={({ features }) => this.setState({ features })}
+            combineFeaturesControl={false}
+            uncombineFeaturesControl={false}
+            lineStringControl={false}
+            pointControl={false}
+            polygonControl={false}
+            trashControl={false}
           />
         </MapGL>
-        {/* <div id="draw">{JSON.stringify(this.state.features)}</div> */}
         <div
           id="tools"
-          style={{ display: this.state.display, overflow: "scroll",float:"left" }}
+          style={{
+            display: this.state.display,
+            overflow: "scroll",
+            float: "left"
+          }}
         >
           <Button compact size="small" onClick={this._undisp}>
             Hide
@@ -154,11 +217,27 @@ class App extends Component {
           <Button compact size="small" onClick={this._onClickDistance}>
             Sum Distance
           </Button>
-          <Label>{this.state.sumarea} Km<sup>2</sup></Label>
+          <Button compact size="small" onClick={this._mode}>
+            L
+          </Button>
+          <Button compact size="small" onClick={this._modearea}>
+            O
+          </Button>
+          <Label>
+            {this.state.sumarea} Km<sup>2</sup>
+          </Label>
           <Label>{this.state.sumdistance} Km</Label>
 
-          <Table collapsing id="tbl" style={{ top: "60px"}}>
-            <Table.Header>
+          <Table
+            collapsing
+            id="tbl"
+            style={{ top: "60px" }}
+            size="small"
+            celled
+            structured
+          >
+            <Table.Header />
+            <Table.Body>
               <Table.Row>
                 <Table.HeaderCell>
                   <center>longitude</center>
@@ -167,12 +246,7 @@ class App extends Component {
                   <center>Langtitude</center>
                 </Table.HeaderCell>
               </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              <Table.Row>
-                <Table.Cell>{(this.state.arrCoord)}</Table.Cell>
-                <Table.Cell />
-              </Table.Row>
+              {this.state.arrTbl}
             </Table.Body>
           </Table>
         </div>
@@ -213,22 +287,21 @@ class App extends Component {
           </Button>
           <Button
             compact
-            disabled
-            value=""
+            value="mapbox://styles/mapbox/navigation-preview-day-v2"
             onClick={this._StyleChange}
             size="small"
           >
-            Terrain
+            Preview
           </Button>
         </div>
 
         <div id="Bottom">
           {/** button navigasi bawah */}
-          <img src="mapid-logo.svg" align="center" height="30px" alt="Logo" />
+          <img src="mapid-logo.svg" align="center" height="30px" alt="logo" />
           <a href="https://www.mapid.io/">
             <Button compact size="small">
-              <img src="geo-icon.png" height="10px" alt="Geo" />
-              <img src="mapid-icon.png" height="10px" alt="Mapid" />
+              <img src="geo-icon.png" height="10px" alt="geo" />
+              <img src="mapid-icon.png" height="10px" alt="mapid" />
             </Button>
           </a>
           <Button
@@ -239,60 +312,30 @@ class App extends Component {
             labelPosition="left"
             onClick={this._disp}
           />
-          <Popup
-            trigger={
-              <Button
-                compact
-                disabled
-                size="small"
-                content="Details"
-                icon="bars"
-                labelPosition="left"
-              />
-            }
-            on="click"
-            className="login-popup"
-          >
-            <div className="popup-main">
-              <Message attached="bottom">This is Details</Message>
-            </div>
-          </Popup>
-          <Popup
-            trigger={
-              <Button
-                compact
-                disabled
-                size="small"
-                content="Inspect"
-                icon="search"
-                labelPosition="left"
-              />
-            }
-            on="click"
-            className="login-popup"
-          >
-            <div className="popup-main">
-              <Message attached="bottom">This is Inspect</Message>
-            </div>
-          </Popup>
-          <Popup
-            trigger={
-              <Button
-                compact
-                disabled
-                size="small"
-                content="Navigate"
-                icon="location arrow"
-                labelPosition="left"
-              />
-            }
-            on="click"
-            className="login-popup"
-          >
-            <div id="show_nav">
-              <Message attached="bottom">This is Navigation</Message>
-            </div>
-          </Popup>
+          <Button
+            compact
+            disabled
+            size="small"
+            content="Details"
+            icon="bars"
+            labelPosition="left"
+          />
+          <Button
+            compact
+            disabled
+            size="small"
+            content="Inspect"
+            icon="search"
+            labelPosition="left"
+          />
+          <Button
+            compact
+            disabled
+            size="small"
+            content="Navigate"
+            icon="location arrow"
+            labelPosition="left"
+          />
           <Label id="isilang" />
         </div>
 
@@ -319,11 +362,11 @@ class App extends Component {
             <Table.Row>
               <Table.Cell collapsing>
                 <Button.Group compact size="small">
-                  <Button disabled>ON</Button>
-                  <Button disabled>OFF</Button>
+                  <Button onClick={this._showmarkerCCTV}>ON</Button>
+                  <Button onClick={this._unshowmarkerCCTV}>OFF</Button>
                 </Button.Group>
               </Table.Cell>
-              <Table.Cell>NowMapidStory</Table.Cell>
+              <Table.Cell>CCTV</Table.Cell>
             </Table.Row>
             <Table.Row>
               <Table.Cell collapsing>
@@ -332,7 +375,7 @@ class App extends Component {
                   <Button disabled>OFF</Button>
                 </Button.Group>
               </Table.Cell>
-              <Table.Cell>Cctv</Table.Cell>
+              <Table.Cell>Banjir</Table.Cell>
             </Table.Row>
             <Table.Row>
               <Table.Cell collapsing>
@@ -341,7 +384,7 @@ class App extends Component {
                   <Button disabled>OFF</Button>
                 </Button.Group>
               </Table.Cell>
-              <Table.Cell>Bendungan_Indonesia</Table.Cell>
+              <Table.Cell>Bendungan</Table.Cell>
             </Table.Row>
           </Table.Body>
         </Table>
