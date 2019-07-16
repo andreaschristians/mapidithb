d09939ea89142f2024a4772684d14f8c9f7ce2a5
Andreas Christian Susanto <andreaschristiansusanto.070499@gmail.com>
2019-07-16 17:12:14 +0700

    navigation UI dan coba buat layer

--- Andreas/03. July/20190705/geomapid/src/App.js
+++ Andreas/03. July/20190705/geomapid/src/App.js
@@ -3,8 +3,6 @@ import * as turf from "@turf/turf"; //Tools bantuan untuk hitung distance dll
 import "mapbox-gl/dist/mapbox-gl.css"; //CSS mapbox
 import React, { Component } from "react"; //React
 import "semantic-ui-css/semantic.min.css"; //CSS Sematic
-import Geocoder from "react-map-gl-geocoder/dist/index.js";
-import "react-map-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import Draw from "@urbica/react-map-gl-draw";
 import "@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css";
 import {
@@ -16,7 +14,163 @@ import {
   Icon,
   Select
 } from "semantic-ui-react"; //Tools bantuan untuk UI
-import MapGL, { GeolocateControl, Marker } from "@urbica/react-map-gl"; //Mapbox Urbica
+import MapGL, {
+  GeolocateControl,
+  Marker,
+  Layer,
+  Source
+} from "@urbica/react-map-gl"; //Mapbox Urbica
+import Geocoder from "react-mapbox-gl-geocoder";
+const databanjir1 = {
+  red: {
+    type: "Feature",
+    geometry: {
+      type: "Polygon",
+      coordinates: [
+        [
+          [106.72718612208746, -6.154339766596152],
+          [106.72721825022546, -6.161750486558091],
+          [106.73361175106402, -6.160728324424994],
+          [106.7387201261073, -6.159067306783072],
+          [106.7475553660123, -6.158140967794381],
+          [106.7481324235614, -6.155170283482676],
+          [106.74308830482175, -6.155042511742067],
+          [106.73447796196547, -6.1547550252202825],
+          [106.73107237862172, -6.155106397616194],
+          [106.72718612208746, -6.154339766596152]
+        ]
+      ]
+    }
+  },
+  pink: {
+    type: "Feature",
+    geometry: {
+      type: "Polygon",
+      coordinates: [
+        [
+          [106.73285781277269, -6.160926327882834],
+          [106.73285874339376, -6.161706411727565],
+          [106.73207366198494, -6.16270377502893],
+          [106.73224812459148, -6.163397374842475],
+          [106.73198643076552, -6.163924704796273],
+          [106.73163750571024, -6.1640981587041495],
+          [106.73102688684628, -6.166092874422276],
+          [106.72989288038224, -6.167697314239788],
+          [106.72923864586909, -6.167914130051997],
+          [106.72923864586909, -6.1685645769738215],
+          [106.72775571434983, -6.168954844750772],
+          [106.72862802700524, -6.176153065444822],
+          [106.72958757094159, -6.1758928904881145],
+          [106.73032903670128, -6.175632715420704],
+          [106.73098327121437, -6.176153065444822],
+          [106.73102688684628, -6.174938914598755],
+          [106.73207366202917, -6.174418563381536],
+          [106.73172473697406, -6.173854848987318],
+          [106.73203004639731, -6.172640692872619],
+          [106.73246620273369, -6.17272741839929],
+          [106.73298959033383, -6.171730073951849],
+          [106.73390551863832, -6.17120971958262],
+          [106.7342544436936, -6.171860162460078],
+          [106.73495229383866, -6.1709929051184815],
+          [106.73530121890343, -6.171122993816027],
+          [106.73582193270767, -6.170646001782501],
+          [106.73560385454823, -6.170255735250819],
+          [106.73586554833963, -6.169735379451126],
+          [106.73643255157174, -6.170429187087706],
+          [106.73734847987623, -6.169908831441134],
+          [106.73778463621255, -6.17055927591521],
+          [106.7385697176041, -6.170255735250819],
+          [106.7377410205633, -6.169258386168693],
+          [106.73808994563586, -6.168607940098099],
+          [106.7385697176041, -6.169648653417582],
+          [106.73904948957232, -6.169215023123286],
+          [106.73979095533196, -6.168130945801167],
+          [106.74031434291652, -6.168607940106966],
+          [106.74061931905914, -6.167610587907305],
+          [106.74118632229107, -6.16787076691395],
+          [106.74170970989127, -6.168087582672371],
+          [106.74240756001899, -6.166830050087185],
+          [106.74415218533943, -6.166439780771995],
+          [106.74410856970758, -6.165745967914518],
+          [106.74319264140303, -6.16539906113691],
+          [106.74227671311576, -6.165529151198555],
+          [106.74188417241135, -6.164748610332225],
+          [106.7424947912753, -6.164531793208667],
+          [106.74184055677938, -6.164054795262615],
+          [106.740750165978, -6.16457515665158],
+          [106.74096824410293, -6.165266654902524],
+          [106.73970339074322, -6.165353381635583],
+          [106.74031400960712, -6.166871097219712],
+          [106.7375662247108, -6.168605623989393],
+          [106.73691199021499, -6.169689700340669],
+          [106.73569075248707, -6.169169343968079],
+          [106.73647583387861, -6.168302082206154],
+          [106.73603967755963, -6.16596046838697],
+          [106.73630137135103, -6.1650064746011],
+          [106.73730453091935, -6.164356023315008],
+          [106.73704283711066, -6.163445390181607],
+          [106.73516736488699, -6.163228572525398],
+          [106.73490567109553, -6.160930300036668],
+          [106.73717368402362, -6.16014975240526],
+          [106.73699922147875, -6.159759478150647],
+          [106.73285781277269, -6.160926327882834]
+        ]
+      ]
+    }
+  },
+  green: {
+    type: "Feature",
+    geometry: {
+      type: "Polygon",
+      coordinates: [
+        [
+          [106.74890628935145, -6.166220648203222],
+          [106.7492988300558, -6.172941916072347],
+          [106.75571032813576, -6.165570198406073],
+          [106.75191576803917, -6.166567554443432],
+          [106.75052006778373, -6.166047195006556],
+          [106.74890628935145, -6.166220648203222]
+        ]
+      ]
+    }
+  }
+};
+const databanjir2 = {
+  red2: {
+    type: "Feature",
+    geometry: {
+      type: "Polygon",
+      coordinates: [
+        [
+          [106.73244138558238, -6.175777656139218],
+          [106.73374986292168, -6.167159231939635],
+          [106.7389837722792, -6.147645301337278],
+          [106.77104146712895, -6.13431037023814],
+          [106.76090076773124, -6.1718749908575035],
+          [106.74012868995112, -6.1843959400458175],
+          [106.73162358724517, -6.187648086157765],
+          [106.73244138558238, -6.175777656139218]
+        ]
+      ]
+    }
+  },
+  pink2: {
+    type: "Feature",
+    geometry: {
+      type: "Polygon",
+      coordinates: [
+        [
+          [106.71444982216627, -6.158134128188621],
+          [106.71706677684921, -6.174070243635086],
+          [106.71150574815567, -6.189355454928673],
+          [106.70366908832625, -6.1990834992753605],
+          [106.68665888289519, -6.186888112114033],
+          [106.71444982216627, -6.158134128188621]
+        ]
+      ]
+    }
+  }
+};
 
 class App extends Component {
   // INISIALISASI UTAMA
@@ -47,6 +201,7 @@ class App extends Component {
       conver: "none",
       details: "none",
       inspect: "none",
+      navigation: "none",
       inputarea: 1,
       inputarea_: 1,
       inputlength: 1,
@@ -54,7 +209,18 @@ class App extends Component {
       outputarea: 1,
       outputarea_: 1,
       outputlength: 1,
-      outputlength_: 1
+      outputlength_: 1,
+      banjir1: 0,
+      banjir2: 0,
+      before: {
+        red: "",
+        pink: "",
+        green: ""
+      },
+      after: {
+        red2: "",
+        pink2: ""
+      }
     };
 
     //DEKLARASI FUNCTION ATAU PROCEDURE
@@ -239,6 +405,38 @@ class App extends Component {
             </Popup>
           </Marker>
 
+          {/* BANJIR1 */}
+          {Object.entries(this.state.before).map(([layerId]) => (
+            <React.Fragment key={layerId}>
+              <Source id={layerId} type="geojson" data={databanjir1[layerId]} />
+              <Layer
+                id={layerId}
+                type="fill"
+                source={layerId}
+                paint={{
+                  "fill-color": "red",
+                  "fill-opacity": this.state.banjir1
+                }}
+              />
+            </React.Fragment>
+          ))}
+
+          {/* BANJIR2 */}
+          {Object.entries(this.state.after).map(([layerId]) => (
+            <React.Fragment key={layerId}>
+              <Source id={layerId} type="geojson" data={databanjir2[layerId]} />
+              <Layer
+                id={layerId}
+                type="fill"
+                source={layerId}
+                paint={{
+                  "fill-color": "blue",
+                  "fill-opacity": this.state.banjir2
+                }}
+              />
+            </React.Fragment>
+          ))}
+
           {/* DRAW PROPERTIES */}
           <Draw
             data={this.state.data}
@@ -252,7 +450,15 @@ class App extends Component {
             polygonControl={false}
             trashControl={false}
           />
-          <div style={{ position: "fixed", top: "15px", right: "50px" }}>
+          <div
+            style={{
+              position: "fixed",
+              top: "15px",
+              right: "50px",
+              maxWidth: "150px",
+              boxShadow: "0 0 10px 2px rgba(0,0,0,.1)"
+            }}
+          >
             <Geocoder
               mapboxApiAccessToken="pk.eyJ1IjoiYW5kcmVhc2NocmlzdGlhbiIsImEiOiJjanZ2cnZhMjg0NWtmNDN1aTMxcGphY21xIn0.CDEBH4hJPmAhRDOtzz73Mw"
               onSelected={this.onSelected}
@@ -628,6 +834,43 @@ class App extends Component {
           </Button.Group>
         </div>
 
+        {/* NAVIGATION */}
+        <div
+          style={{
+            position: "fixed",
+            backgroundColor: "rgba(113, 124, 124, 0.7)",
+            bottom: "70px",
+            right: "10px",
+            height: "150px",
+            width: "300px",
+            display: this.state.navigation
+          }}
+        >
+          
+          {/* CLOSE BUTTON */}
+          <Button
+            compact
+            color="red"
+            size="small"
+            onClick={() =>
+              this.setState({
+                navigation: "none"
+              })
+            }
+            style={{
+              position: "fixed",
+              right: "7px",
+              bottom: "218px",
+              color: "white",
+              opacity: "0.7"
+            }}
+          >
+            X
+          </Button>
+
+          <center>NAVIGATION</center>
+        </div>
+
         {/** CHANGE STYLE MAPBOX */}
         <div
           style={{
@@ -698,10 +941,10 @@ class App extends Component {
               onClick={() => this.setState({ inspect: "inline-block" })}
             />
             <Button
-              disabled
               content="Navigate"
               icon="location arrow"
               labelPosition="left"
+              onClick={() => this.setState({ navigation: "inline-block" })}
             />
             <Button id="isilang" />
           </Button.Group>
@@ -762,20 +1005,32 @@ class App extends Component {
             <Table.Row>
               <Table.Cell collapsing>
                 <Button.Group compact size="mini">
-                  <Button disabled icon="eye" />
-                  <Button disabled icon="eye slash" />
+                  <Button
+                    icon="eye"
+                    onClick={() => this.setState({ banjir1: 0.5 })}
+                  />
+                  <Button
+                    icon="eye slash"
+                    onClick={() => this.setState({ banjir1: 0 })}
+                  />
                 </Button.Group>
               </Table.Cell>
-              <Table.Cell>Banjir</Table.Cell>
+              <Table.Cell>Banjir 1990</Table.Cell>
             </Table.Row>
             <Table.Row>
               <Table.Cell collapsing>
                 <Button.Group compact size="mini">
-                  <Button disabled icon="eye" />
-                  <Button disabled icon="eye slash" />
+                  <Button
+                    icon="eye"
+                    onClick={() => this.setState({ banjir2: 0.5 })}
+                  />
+                  <Button
+                    icon="eye slash"
+                    onClick={() => this.setState({ banjir2: 0 })}
+                  />
                 </Button.Group>
               </Table.Cell>
-              <Table.Cell>Bendungan</Table.Cell>
+              <Table.Cell>Banjir 2000</Table.Cell>
             </Table.Row>
           </Table.Body>
         </Table>
